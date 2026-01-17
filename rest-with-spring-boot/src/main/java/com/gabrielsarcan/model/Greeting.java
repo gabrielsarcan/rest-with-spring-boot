766fcb17626cba000f44a802df60e7122026fafe
gabrielsarcan <gabrielsarcan@hotmail.com>
2026-01-17 14:36:26 -0300

    preparando a api

---
+++ rest-with-spring-boot/src/main/java/com/gabrielsarcan/model/Greeting.java
@@ -0,0 +1,5 @@
+package com.gabrielsarcan.model;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ rest-with-spring-boot/src/test/java/com/gabrielsarcan/startup.java
@@ -0,0 +1,13 @@
+ package com.gabrielsarcan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class startup {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
